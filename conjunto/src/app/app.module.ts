cd73a4f50878ea61c690d86d8ae669b6d8e298d9
americoafonso <americo.quim@hotmail.com>
2020-06-13 00:47:22 -0300

    Implementação do componente conjunto.
    Implementação do componente conjunto e responsividade do layout.

--- conjunto/src/app/app.module.ts
+++ conjunto/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+import {TooltipModule} from 'primeng/tooltip';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConjuntoComponent } from './conjunto/conjunto.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConjuntoComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    InputTextModule,
+    ButtonModule,
+    TooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ conjunto/src/app/conjunto/conjunto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-conjunto',
+  templateUrl: './conjunto.component.html',
+  styleUrls: ['./conjunto.component.css']
+})
+export class ConjuntoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
