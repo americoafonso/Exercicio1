d3d277386afbd9dd206eedb109372fc351c6b37c
americoafonso <americo.quim@hotmail.com>
2020-06-13 16:51:34 -0300

    Implementação do método de saida.
    Implementação do método de saida, que devolve somente os valores vàlidos.

--- conjunto/src/app/app.module.ts
+++ conjunto/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {InputTextModule} from 'primeng/inputtext';
 import {ButtonModule} from 'primeng/button';
 import {TooltipModule} from 'primeng/tooltip';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +25,11 @@ import { NavbarComponent } from './navbar/navbar.component';
 
     InputTextModule,
     ButtonModule,
-    TooltipModule
+    TooltipModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- conjunto/src/app/conjunto/conjunto.component.ts
+++ conjunto/src/app/conjunto/conjunto.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-conjunto',
@@ -7,7 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConjuntoComponent implements OnInit {
 
-  constructor() { }
+  public form: FormGroup;
+
+  public entrada: string;
+  public saida: string;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      entrada: ['', Validators.required],
+      saida: ['', Validators.required]
+    });
+  }
+
+  ordenaNumber = function (a: number, b: number) {
+    return a - b;
+  }
+
+  resultado(entrada) {
+    let entradaFormatada = this.entrada.split(' ');
+    let arrayTemporario: any = [];
+    let itemFinal: any = [];
+
+    for (let i = 0; i <= entradaFormatada.length - 1; i++) {
+      if (entradaFormatada[i]) {
+        arrayTemporario = parseInt(entradaFormatada[i]);
+        if (arrayTemporario <= 1000 && arrayTemporario >= -1000 && !itemFinal.includes(arrayTemporario)) {
+          itemFinal.push(arrayTemporario);
+          itemFinal.sort(this.ordenaNumber);
+        }
+      }
+    }
+    this.saida = itemFinal.toString().split(',').join(' ');
+
+  }
 
   ngOnInit() {
   }
